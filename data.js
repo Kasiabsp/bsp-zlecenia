02a2938a418b01f37e2f3056ce843e8b7a3af1cc
Kasiabsp <jakubsoperatorbsp@gmail.com>
2026-03-12 15:51:57 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+window.BSP_ANNOUNCEMENTS = [
+  {
+    date: "2026-03-12T15:45:00Z",
+    text: "Informujemy, że w dniach 13–15 marca 2026 r. usługi BSP Zlecenia nie będą dostępne z powodu prac serwisowych.",
+    expires: "2026-03-16T00:00:00Z"
+  },
+  {
+    date: "2026-03-12T15:46:00Z",
+    text: "W dniach 20–22 marca 2026 r. planowana jest przerwa techniczna. Usługi BSP Zlecenia będą w tym czasie niedostępne.",
+    expires: "2026-03-23T00:00:00Z"
+  }
+];
